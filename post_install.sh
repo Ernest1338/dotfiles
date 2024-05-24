9fee0614777ac8fc265157ee45ea1db0e6dd4b6f
Ernest Gupik <ernestgupik@wp.pl>
2024-05-24 21:48:23 +0200

    Add mako + bash and post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,7 +1,59 @@
 #!/bin/bash
 
 # install additional packages
-# neovim chezmoi hyprland hyprlock git base-devel rustup meson cmake cpio lazygit ttf-hack alacritty
+sudo pacman -S --noconfirm --needed \
+    neovim \
+    chezmoi \
+    neovim \
+    chezmoi \
+    hyprland \
+    hyprlock \
+    git \
+    base-devel \
+    rustup \
+    meson \
+    cmake \
+    cpio \
+    lazygit \
+    ttf-hack \
+    alacritty \
+    imv \
+    mpv \
+    ly \
+    i2c-tools \
+    wget \
+    htop \
+    ripgrep \
+    fd \
+    ncdu \
+    wl-clipboard \
+    fuzzel \
+    waybar \
+    mako \
+    thunar \
+    grim \
+    slurp \
+    python \
+    python-lsp-server \
+    python-lsp-black \
+    uv \
+    qemu-base \
+    ffmpeg \
+    lua-language-server \
+    pavucontrol
+
+# TODO (only needed) install fonts: ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-common ttf-nerd-fonts-mono
+
+# on laptop: brightnessctl tlp
+# write tlp config > /etc/tlp.conf
+# CPU_ENERGY_PERF_POLICY_ON_AC=performance
+# CPU_ENERGY_PERF_POLICY_ON_BAT=power
+# CPU_MAX_PERF_ON_AC=100
+# CPU_MAX_PERF_ON_BAT=20
+# CPU_MIN_PERF_ON_AC=0
+# CPU_MIN_PERF_ON_BAT=0
+# CPU_SCALING_GOVERNOR_ON_AC=performance
+# CPU_SCALING_GOVERNOR_ON_BAT=powersave
 
 # setup paru
 
@@ -9,5 +61,15 @@
 # brave-bin hyprpicker
 
 # chezmoi init https://github.com/Ernest1338/dotfiles.git
+
+# setup git (username, useremail, credential helper store)
+
+# systemctl --user enable --now mako
+# for waybar: create new under .config (save with chezmoi)
+# systemctl enable ly
+
+# network manager set dns to 1.1.1.1 and 9.9.9.9
+
+# xhost + local:
 
 # install neovim config
