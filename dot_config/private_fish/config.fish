08fa451e9b070768c0432df475abcd8a8be331e6
Ernest Gupik <ernestgupik@wp.pl>
2024-09-05 20:28:02 +0200

    Fish: add more binds

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -38,9 +38,12 @@ if status is-interactive
     alias gamingcont="podman run -it -e DISPLAY --ipc=host --device /dev/dri/card1 -v /dev/input:/dev/input -v /etc/machine-id:/etc/machine-id -v /run/dbus/system_bus_socket:/run/dbus/system_bus_socket -v /tmp/.X11-unix:/tmp/.X11-unix --device /dev/snd -e PULSE_SERVER=unix:$XDG_RUNTIME_DIR/pulse/native -v $XDG_RUNTIME_DIR:$XDG_RUNTIME_DIR --group-add audio -v ~/.config/pulse/cookie:/root/.config/pulse/cookie --privileged --name gaming_container archlinux"
     alias archvm="podman run --rm -it -e DISPLAY --ipc=host --device /dev/dri/card1 -v /dev/input:/dev/input -v /etc/machine-id:/etc/machine-id -v /run/dbus/system_bus_socket:/run/dbus/system_bus_socket -v /tmp/.X11-unix:/tmp/.X11-unix --device /dev/snd -e PULSE_SERVER=unix:$XDG_RUNTIME_DIR/pulse/native -v $XDG_RUNTIME_DIR:$XDG_RUNTIME_DIR --group-add audio -v ~/.config/pulse/cookie:/root/.config/pulse/cookie --privileged archlinux"
     alias hackingvm="pushd $HOME/Files/VMs && ./hackingvm.sh && popd"
+    alias lg=lazygit
 
     bind \ck up-or-search
     bind \cj down-or-search
+    bind \ch backward-word
+    bind \cl forward-word
 end
 
 function screenrecord
