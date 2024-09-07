6228620b56e3b157d863554691326431c7d7a00f
Ernest Gupik <ernestgupik@wp.pl>
2024-09-07 02:08:38 +0200

    Fish: note theme and prompt

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,4 +1,5 @@
 if status is-interactive
+    # NOTE: theme: ayu Mirage, prompt: hydro
     set -g fish_greeting
     set -gx XDG_DATA_HOME "$HOME/.local/share"
     set -gx XDG_CONFIG_HOME "$HOME"/.config
